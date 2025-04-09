0f02b8e48789f68a4bd40244238402106b726825
Guadalupe Vega <guadalupe.vega@mi.unc.edu.ar>
2025-04-08 21:29:31 -0300

    change folder name

---
+++ TP2/Segunda_lteracion/procesamiento.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Declaración externa
+extern int float_to_int(float);
+
+// Función para procesar el valor de Gini
+int procesar_gini(float gini_float) {
+    return float_to_int(gini_float);
+}
+
