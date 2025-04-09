d9d9cee721b676e4f6b6c87eec85e21bd0d90174
Guadalupe Vega <guadalupe.vega@mi.unc.edu.ar>
2025-04-08 22:00:34 -0300

    fix folders

--- TP2/PrimeraIteracion/src/c/procesamiento.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-// Funcion para procesar el valor de Gini
-int procesar_gini(float gini_float) {
-    int gini_entero = (int)round(gini_float);  // Convertir a entero
-    return gini_entero + 1;  // Sumar 1
-}
