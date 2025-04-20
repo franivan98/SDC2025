300b7a6d9671fd27f47d4173af89e87a75d12440
Lenox30 <lenox.graham@mi.unc.edu.ar>
2025-04-20 20:52:47 -0300

    Se agrega el .MD con instrucciones

--- TP2/Primera_Iteracion/src/python/main.py
+++ TP2/Primera_Iteracion/src/python/main.py
@@ -8,12 +8,10 @@ lib_procesamiento.procesar_gini.argtypes = [ctypes.c_float]
 # Definimos el tipo de retorno de la función
 lib_procesamiento.procesar_gini.restype = ctypes.c_int
 
-def obtener_gini():
-    # Obtener el índice de Gini de Argentina
-    return obtener_gini_argentina()
 
 # Obtener y procesar GINI
-gini_float = obtener_gini()
+gini_float = obtener_gini_argentina()
+# Verificamos si se obtuvo un valor válido
 if gini_float is not None:
     resultado = lib_procesamiento.procesar_gini(gini_float)
     print(f"Índice GINI procesado: {resultado}")
